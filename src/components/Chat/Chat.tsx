495dee219e152dc3a56ea7db327d759982f0b9a5
SJ <sotsu@adachi-printing.co.jp>
2021-06-18 11:00:50 +0900

    ウィンドウを閉じるなどした場合、退室をチェックできない不具合を調整

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -20,26 +20,33 @@ const Chat = () => {
             const { uid, displayName, photoURL } = currentUser
             dispatch({ type: 'setActive', payload: { id: uid, name: displayName, photoURL: photoURL } })
         }
+
+        window.addEventListener("beforeunload", () => dispatch({ type: 'inActive' }))
+
         return () => {
             dispatch({ type: 'inActive' })
+            window.removeEventListener("beforeunload", () => dispatch({ type: 'inActive' }))
         }
     }, [currentUser])
 
-    return (
-        <>
-            <StyledSideActions>
-                <ChannelSelector />
-            </StyledSideActions>
-            <StyledMain>
-                <Typography variant="h4">{state.channel?.name}</Typography>
-                <InputMessage />
-                <StyledTimeline>
-                    <Timeline />
-                </StyledTimeline>
-            </StyledMain>
-            <ActiveUserList />
-        </>
-    )
-}
+    useEffect(() => {
+    }, [])
+
+        return (
+            <>
+                <StyledSideActions>
+                    <ChannelSelector />
+                </StyledSideActions>
+                <StyledMain>
+                    <Typography variant="h4">{state.channel?.name}</Typography>
+                    <InputMessage />
+                    <StyledTimeline>
+                        <Timeline />
+                    </StyledTimeline>
+                </StyledMain>
+                <ActiveUserList />
+            </>
+        )
+    }
 
 export default Chat
